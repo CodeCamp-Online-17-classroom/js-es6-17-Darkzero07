c98f9f91a5ecf395aa9274f1993e969a3cb1bf2a
Darkzero7 <chaiyanituporn@gmail.com>
2024-07-01 00:03:24 +0700

    js lab 161

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+// ให้ใช้ Array destructuring ในการสลับค่าตัวแปร guest กับ admin
+
+let guest = 'Jane';
+let admin = 'Pete';
+
+[guest, admin] =[admin, guest]
+console.log(guest)
+console.log(admin)
